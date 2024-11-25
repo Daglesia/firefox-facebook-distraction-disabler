03aefd173bce46c35c97692759c4146b08181a36
Daglesia <daglesia.gaming@gmail.com>
2024-11-25 19:18:32 +0100

    (feat): Initial version

---
+++ main.js
@@ -0,0 +1,31 @@
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+function waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+const banner = document.querySelector('div[role="banner"]');
+banner.style.display = 'none';
+
+const notificationButton = document.querySelector('a[href="/notifications/"]').parentElement;
+
+waitForElement('div[aria-label="Settings, help and more"').then((element) => {
+    const chatMenu = element.parentElement.parentElement;
+
+    chatMenu.appendChild(notificationButton);
+    notificationButton.style.marginLeft = '8px';
+});
